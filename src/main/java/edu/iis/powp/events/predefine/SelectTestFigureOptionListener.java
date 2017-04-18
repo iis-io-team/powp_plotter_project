5bceb5932760961abb44f627059d061c28834137
Maciek <miedzianowski.maciek@gmail.com>
2017-04-18 19:36:43 +0200

    Dodanie do GUI testu rysującego "FigureJane"

--- src/main/java/edu/iis/powp/events/predefine/SelectTestFigureOptionListener.java
+++ src/main/java/edu/iis/powp/events/predefine/SelectTestFigureOptionListener.java
@@ -1,11 +1,13 @@
 package edu.iis.powp.events.predefine;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import edu.iis.client.plottermagic.preset.FiguresJane;
 import edu.iis.client.plottermagic.preset.FiguresJoe;
+import edu.iis.powp.adapter.SpecialAdapter;
 import edu.iis.powp.app.Application;
 import edu.iis.powp.app.DriverManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SelectTestFigureOptionListener implements ActionListener
 {
@@ -18,6 +20,9 @@ public class SelectTestFigureOptionListener implements ActionListener
         }
         else if(e.getActionCommand().equals("Figure Joe 2")) {
             FiguresJoe.figureScript2(Application.getComponent(DriverManager.class).getCurrentPlotter());
+        }
+        else if(e.getActionCommand().equals("Figure Jane 1")) {
+            FiguresJane.figureScript(new SpecialAdapter(0,0));
         }
 
     }
